e7192f7e5c69b8c3a3e2904811f5c2d7a8410b74
hanyoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-10-28 16:49:13 +0900

    status

--- src/app.jsx
+++ src/app.jsx
@@ -5,13 +5,51 @@ import Subject from "./components/subject";
 import Content from "./components/content";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: "welcome",
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      welcome: { title: "welcome", desc: "Hello, React!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is..." },
+        { id: 2, title: "CSS", desc: "CSS is..." },
+        { id: 3, title: "JavaScript", desc: "JavaScript is..." },
+      ],
+    };
+  }
+
   render() {
+    let _title, _desc;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="app">
-        <Subject title="WEB" sub="world wide web!" />
-        <Subject title="React" sub="for UI" />
-        <TOC />
-        <Content title="HTML" description="HTML is HyperText Markup Language" />
+        {/* <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        /> */}
+        <header>
+          <h1>
+            <a
+              href="/"
+              onClick={(e) => {
+                console.log(e);
+                e.preventDefault();
+              }}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
+        <TOC data={this.state.contents} />
+        <Content title={_title} description={_desc} />
       </div>
     );
   }
--- src/components/toc.jsx
+++ src/components/toc.jsx
@@ -2,19 +2,18 @@ import { Component } from "react";
 
 class TOC extends Component {
   render() {
+    const lists = [];
+    const contents = this.props.data;
+    contents.forEach((el) => {
+      lists.push(
+        <li key={el.id}>
+          <a href={"/content/" + el.id}>{el.title}</a>
+        </li>
+      );
+    });
     return (
       <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
+        <ul>{lists}</ul>
       </nav>
     );
   }
